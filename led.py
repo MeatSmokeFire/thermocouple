96df05a350250e42029ab1bd7321b815b51c4d50
MeatSmokeFire <nic@meatsmaokefire.co.uk>
2017-10-16 16:48:04 +0000

    add README.md

---
+++ led.py
@@ -0,0 +1,12 @@
+#import the GPIO and time package
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+# loop through 50 times, on/off for 1 second
+for i in range(50):
+    GPIO.output(7,True)
+    time.sleep(1)
+    GPIO.output(7,False)
+    time.sleep(1)
+GPIO.cleanup()
